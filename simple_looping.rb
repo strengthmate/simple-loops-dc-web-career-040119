68010d9081943fede86ac76d89e25b78d78904a0
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-18 14:27:36 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -5,9 +5,8 @@ def loop_iterator(number_of_times)
     loop do 
       counter += 1
       puts "Welcome to Flatiron School's Web Development Course!"
-        if counter <= number_of_times 
-        break
-        end 
+      if counter <= number_of_times 
+      break
     end
 end
 
