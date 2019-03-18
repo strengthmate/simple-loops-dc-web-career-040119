a2cf059e390c506693674741f96d39fe850e2bcf
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-18 14:37:52 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -43,7 +43,7 @@ end
 
 
 
-def for_iterator
+def for_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   checklist = 0..number_of_times
   for all in checklist do
