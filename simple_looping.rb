6e8ac225c44aef7689071a25226f8f4de0cdaef7
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-18 14:29:43 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,15 +1,15 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
-def loop_iterator(number_of_times)
-    counter = 0
-    loop do 
-      counter += 1
-      puts "Welcome to Flatiron School's Web Development Course!"
-      if counter <= number_of_times 
-      break
-    end
-end
+  phrase = "Welcome to Flatiron School's Web Development Course!"
+  
+  counter = 0
+  loop do 
+    puts phrase
+    counter += 1
+    break if counter == number_of_times
+  end
 
+end
 
 def times_iterator(number_of_times)
     number_of_times.times do
