683ef8c33dfbf84c858e06f2aabe966278e8ab25
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-18 14:32:00 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -33,8 +33,9 @@ end
  
 
 def until_iterator(number_of_times)
+  phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 0
-  until a > number_of_times do
+  until counter > number_of_times do
     puts "Welcome to Flatiron School's Web Development Course!"
     counter = counter + 1
     break if counter == number_of_times
